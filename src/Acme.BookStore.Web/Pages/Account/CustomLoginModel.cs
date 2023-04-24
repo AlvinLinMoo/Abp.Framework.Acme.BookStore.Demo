c3caefc52eb4b529db995107cbebdd6e0daf2c83
Alvin Lin <alvin8917@gmail.com>
2023-04-24 15:16:06 -0700

    1. Customize Login Page Model and Login.cshtml 2. Set MultiTenancyConsts IsEnabled = false

---
+++ src/Acme.BookStore.Web/Pages/Account/CustomLoginModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using Volo.Abp.Account.Web;
+using Volo.Abp.Account.Web.Pages.Account;
+
+namespace Acme.BookStore.Web.Pages.Account
+{
+    public class CustomLoginModel : LoginModel
+    {
+        public CustomLoginModel(IAuthenticationSchemeProvider schemeProvider, IOptions<AbpAccountOptions> accountOptions, IOptions<IdentityOptions> identityOptions) : base(schemeProvider, accountOptions, identityOptions)
+        {
+        }
+
+        // Overwrtie your Post or Get Here
+
+
+        // Post Handler: SSO
+        public async Task<IActionResult> OnPostSSOLoginAsync()
+        {
+            Alerts.Warning("Advantech SSO Login is not implemented yet!");
+
+            return Page();
+        }
+
+    }
+}
