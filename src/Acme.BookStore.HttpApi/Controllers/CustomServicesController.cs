7aefb2cce2dca06594a5432bd898687821d9b1c9
Alvin Lin <alvin8917@gmail.com>
2023-05-23 12:33:08 -0700

    1. Update something.

--- src/Acme.BookStore.HttpApi/Controllers/CustomServicesController.cs
+++ src/Acme.BookStore.HttpApi/Controllers/CustomServicesController.cs
@@ -16,6 +16,7 @@ using Acme.BookStore.Permissions;
 
 namespace Acme.BookStore.Controllers
 {
+    [Microsoft.AspNetCore.Mvc.Route("api/app/[controller]")]
     public class CustomServicesController : BookStoreController, IApplicationService
     {
         private readonly IBookAppService _bookAppService;
@@ -28,18 +29,22 @@ namespace Acme.BookStore.Controllers
         }
 
         [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("GetAuthorLookup")]
         public async Task<ActionResult> GetAuthorLookupAsync()
         {
             return new JsonResult(await _bookAppService.GetAuthorLookupAsync());
         }
 
         [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("GetListByAuthorId")]
         public async Task<ActionResult> GetListByAuthorIdAsync(GetBookListByAuthorIdDto input)
         {
             return new JsonResult(await _bookAppService.GetListByAuthorIdAsync(input));
         }
 
-        [HttpPost("api/app/custom-services/CreateAuthor")]
+        //[HttpPost("api/app/custom-services/CreateAuthor")]
+        [HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("CreateAuthor")]
         public async Task<ActionResult> CreateAuthorAsync([FromBody] CreateAuthorDto input)
         {
             var temp = await _authorAppService.CreateAsync(input);
@@ -47,7 +52,9 @@ namespace Acme.BookStore.Controllers
             return new JsonResult(temp);
         }
 
-        [HttpPut("api/app/custom-services/UpdateBook/{id}")]
+        //[HttpPut("api/app/custom-services/UpdateBook/{id}")]
+        [HttpPut]
+        [Microsoft.AspNetCore.Mvc.Route("UpdateBook")]
         public async Task<ActionResult> UpdateBookAsync(Guid id, [FromBody] CreateUpdateBookDto input)
         {
             var temp = await _bookAppService.UpdateAsync(id, input);
